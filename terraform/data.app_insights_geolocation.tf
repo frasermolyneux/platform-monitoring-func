78a4003cd8966bb671c82bc8960f88ec8b581307
Fraser Molyneux <frasermolyneux@mx-mail.io>
2024-07-25 20:52:30 +0100

    Adding providers

--- terraform/data.app_insights_geolocation.tf
+++ terraform/data.app_insights_geolocation.tf
@@ -1,5 +1,7 @@
 
 data "azurerm_application_insights" "geolocation" {
+  provider = azurerm.geolocation
+
   name                = var.geolocation_app_insights.name
   resource_group_name = var.geolocation_app_insights.resource_group_name
 }
--- terraform/data.app_insights_portal.tf
+++ terraform/data.app_insights_portal.tf
@@ -1,5 +1,7 @@
 
 data "azurerm_application_insights" "portal" {
+  provider = azurerm.portal
+
   name                = var.portal_app_insights.name
   resource_group_name = var.portal_app_insights.resource_group_name
 }
