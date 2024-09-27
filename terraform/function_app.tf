2836ddb57c13db98db03e26e4a23b689315e4757
Fraser Molyneux <frasermolyneux@mx-mail.io>
2024-09-27 23:25:33 +0100

    Refactoring permissions

--- terraform/function_app.tf
+++ terraform/function_app.tf
@@ -49,11 +49,3 @@ resource "azurerm_linux_function_app" "app" {
     "DiagnosticServices_EXTENSION_VERSION" = "~3"
   }
 }
-
-resource "azurerm_role_assignment" "app-to-storage" {
-  for_each = toset(var.locations)
-
-  scope                = azurerm_storage_account.function_app_storage[each.value].id
-  role_definition_name = "Storage Blob Data Owner"
-  principal_id         = azurerm_linux_function_app.app[each.value].identity[0].principal_id
-}
