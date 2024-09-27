5cce16837363c4cddb4b3a0d50c21e0d033333ac
Fraser Molyneux <frasermolyneux@mx-mail.io>
2024-09-27 23:13:02 +0100

    Improve security for fa to storage account

--- terraform/function_app.tf
+++ terraform/function_app.tf
@@ -9,8 +9,8 @@ resource "azurerm_linux_function_app" "app" {
 
   service_plan_id = azurerm_service_plan.sp[each.value].id
 
-  storage_account_name       = azurerm_storage_account.function_app_storage[each.value].name
-  storage_account_access_key = azurerm_storage_account.function_app_storage[each.value].primary_access_key
+  storage_account_name          = azurerm_storage_account.function_app_storage[each.value].name
+  storage_uses_managed_identity = true
 
   https_only = true
 
